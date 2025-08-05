a9b76bb275d03c6257da1faa17e5abdeb0c9016d
kumanamu <serachstock1@gmail.com>
2025-08-05 16:27:00 +0900

    새작업

---
+++ src/main/java/com/my/thymeleftTest/controller/mainController.java
@@ -0,0 +1,20 @@
+package com.my.thymeleftTest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainController {
+    // http://localhost:8080/main
+    // http://localhost:8080/
+    @GetMapping({"main", "/"})
+    public String mainPage() {
+        return "mainPage";
+    }
+
+    @GetMapping("/link")
+    public String linkPage(){
+        return "/link/link_test";
+    }
+}
+
